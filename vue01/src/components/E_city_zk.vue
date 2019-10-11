4196b9150259eeb0e4c86e6f88587f5c3ee443da
KAYPC\Kay <k9918@qq.com>
2019-10-11 10:31:24 +0800

    no message

--- vue01/src/components/E_city_zk.vue
+++ vue01/src/components/E_city_zk.vue
@@ -1,13 +1,28 @@
 <template>
-    <div></div>
+  <div id="city">
+    <div id="nav">
+      <span>ele.me</span>
+      <span>登录|注册</span>
+    </div>
+    <p class="title">当前定位城市:</p>
+    <hr>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "E_city_zk"
-    }
+  export default {
+    name: "E_city_zk"
+  }
 </script>
 
 <style scoped>
+  #nav {
 
+    background: rgb(49, 144, 232);
+  }
+
+  #city {
+    color: white;
+    font-size: 19%;
+  }
 </style>
